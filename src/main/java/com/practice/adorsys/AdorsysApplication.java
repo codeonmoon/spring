798d447225d844a2b474da44a63e400d2d16812a
manjari09 <manjarikumari@MacBook.local>
2020-07-30 15:24:33 +0200

    Initial Commit

---
+++ src/main/java/com/practice/adorsys/AdorsysApplication.java
@@ -0,0 +1,13 @@
+package com.practice.adorsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdorsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdorsysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/adorsys/AdorsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.adorsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdorsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
